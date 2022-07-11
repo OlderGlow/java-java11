56f314762a82e4cada99fc55f6f1be11fd59d324
Julien Picquet <julienpicquet1@gmail.com>
2022-07-11 17:13:37 +0200

    add tp function

--- src/test/java/java8/ex04/Function_04_Test.java
+++ src/test/java/java8/ex04/Function_04_Test.java
@@ -30,7 +30,9 @@ public class Function_04_Test {
     // tag::adult[]
     // TODO Compléter la fonction
     // TODO AGE >=18
-    Predicate<Person> adult = null;
+    Predicate<Person> adult = (Person p) -> {
+        return p.getAge() >= 18;
+    };
     // end::adult[]
 
     @Test
@@ -39,7 +41,7 @@ public class Function_04_Test {
         List<Person> personList = Data.buildPersonList();
 
         // TODO invoquer la méthode filter pour que le test soit passant
-        List<Person> result = null;
+        List<Person> result = filter(personList, adult);
 
         assert result.size() == 4;
 
